6f7b90d49a2f12e9382edf0045f09ce54ede6c5b
Samar Abbas <samarabbas172003@gmail.com>
2023-12-29 21:46:46 +0530

    The Last Commit

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   //Features
+//   var featuresLink = document.getElementById("Features-nav");
+//   var featuresSection = document.querySelector(".features-container");
+//   featuresLink.addEventListener("click", function () {
+//     featuresSection.scrollIntoView({ behavior: "smooth" });
+//   });
+
+//   //Faculty
+//   var facultyLink = document.getElementById("Faculty-nav");
+//   var facultySection = document.querySelector(".faculty");
+//   facultyLink.addEventListener("click", function () {
+//     facultySection.scrollIntoView({ behavior: "smooth" });
+//   });
+//   //Gallery
+//   var galleryLink = document.getElementById("Gallery-nav");
+//   var gallerySection = document.querySelector(".gallery");
+//   galleryLink.addEventListener("click", function () {
+//     gallerySection.scrollIntoView({ behavior: "smooth" });
+//   });
+
+//   //FAQ
+//   var faqLink = document.getElementById("FAQ'S-nav");
+//   var faqSection = document.querySelector(".faq");
+//   faqLink.addEventListener("click", function () {
+//     faqSection.scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+document.addEventListener("scroll", function () {
+  var navbar = document.getElementById("scroll");
+  var scrolled = window.scrollY > 100;
+
+  if (scrolled) {
+    navbar.classList.add("transparent");
+  } else {
+    navbar.classList.remove("transparent");
+  }
+});
 document.addEventListener("DOMContentLoaded", function () {
-  //Features
-  var featuresLink = document.getElementById("Features-nav");
-  var featuresSection = document.querySelector(".features-container");
-  featuresLink.addEventListener("click", function () {
-    featuresSection.scrollIntoView({ behavior: "smooth" });
-  });
+  var dropdownIcon = document.querySelector(".dropdown-icon");
+  var navbarList = document.querySelector(".left ul");
 
-  //Faculty
-  var facultyLink = document.getElementById("Faculty-nav");
-  var facultySection = document.querySelector(".faculty");
-  facultyLink.addEventListener("click", function () {
-    facultySection.scrollIntoView({ behavior: "smooth" });
-  });
-  //Gallery
-  var galleryLink = document.getElementById("Gallery-nav");
-  var gallerySection = document.querySelector(".gallery");
-  galleryLink.addEventListener("click", function () {
-    gallerySection.scrollIntoView({ behavior: "smooth" });
-  });
-
-  //FAQ
-  var faqLink = document.getElementById("FAQ'S-nav");
-  var faqSection = document.querySelector(".faq");
-  faqLink.addEventListener("click", function () {
-    faqSection.scrollIntoView({ behavior: "smooth" });
+  dropdownIcon.addEventListener("click", function () {
+    navbarList.classList.toggle("show");
   });
 });
 
@@ -32,10 +50,9 @@ function openNewTab() {
   window.open(imageUrl, "_blank");
 }
 
-//
-function openCourseTemplate() {
-  var imageUrl = "./Images/course.jpg";
-  window.open(imageUrl, "_blank");
+function openNewTab2() {
+  var imageUrl2 = "./Images/abc.jpg";
+  window.open(imageUrl2, "_blank");
 }
 function Go_on_Map() {
   var url = "https://maps.app.goo.gl/gFHy5F9Ksp1CdWBo8";
@@ -47,19 +64,21 @@ function openInsta() {
   window.open(imageUrl, "_blank");
 }
 
-function openEmail() {
-  var emailAddress = "aligariansvisionaryclasses@gmail.com";
-  var subject = "";
-  var mailtoLink =
-    "mailto:" + emailAddress + "?subject=" + encodeURIComponent(subject);
-  window.open(mailtoLink, "_blank");
-}
-
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
-//RESPONSIVENESS
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+});
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "210px";
@@ -69,10 +88,4 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
-}
-
-//regsitration
-function openRegistration() {
-  var Url = "student_registration.html";
-  window.open(Url, "_blank");
 }
